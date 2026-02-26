daf4c375b2d05cc8d2d161cc4d64ce9c24ac25ab
amadansi26-cmyk <amadansi26@bentleyschool.org>
2026-02-26 11:04:50 -0800

    Update CSP_5_02_Sorting.py

--- CSP_5_02_Sorting.py
+++ CSP_5_02_Sorting.py
@@ -5,8 +5,8 @@ def bubbleSort(items):
     comparisons = 0
     n = len(items)
 
-    for i in range(n):           
-        for j in range(n - 1):
+    for i in range(n):
+        for j in range(n - 1 - i):
             comparisons += 1
             if items[j] > items[j + 1]:
                 items[j], items[j + 1] = items[j + 1], items[j]
